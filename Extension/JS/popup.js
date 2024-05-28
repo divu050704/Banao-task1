17d1f75b0caa1213b411a99eccb600e8964d6ffe
divu050704 <divyanshu14472@gmail.com>
2024-05-28 23:27:03 +0530

    If pushed this commit will remove extra message being sent between content file and background file

--- Extension/JS/popup.js
+++ Extension/JS/popup.js
@@ -1,6 +1,20 @@
 window.onload = () => {
     const button = document.getElementById("retrieve-title-button");
     button.addEventListener("click", async function () {
-        await chrome.runtime.sendMessage({action: "filter"})
+        const array = ["https://www.linkedin.com/in/nakul-ghai-8741901aa/", "https://www.linkedin.com/in/vineet-kankerwal-11145b260/", "https://www.linkedin.com/in/priyamgupta2806/"]
+        array.map(async (ele) => {
+            chrome.tabs.create({
+                url: ele
+            }, async function (newTab) {
+                chrome.scripting
+                    .executeScript({
+                        target: { tabID: newTab.id },
+                        files: ["./JS/content.js"],
+                    })
+                await new Promise(r => setTimeout(r, 7000));
+
+            })
+
+        })
     })
 }
